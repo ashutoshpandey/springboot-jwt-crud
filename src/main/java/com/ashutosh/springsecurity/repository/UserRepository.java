4dc595a34aae7255596d7eac3e0c6d6a51eb25af
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2023-08-15 08:56:11 +0530

    Added like query

--- src/main/java/com/ashutosh/springsecurity/repository/UserRepository.java
+++ src/main/java/com/ashutosh/springsecurity/repository/UserRepository.java
@@ -2,14 +2,22 @@ package com.ashutosh.springsecurity.repository;
 
 import com.ashutosh.springsecurity.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByName(String name);
-    List<User> getUsersByName(String name);
-    List<User> getUsersByEmail(String email);
     Optional<User> findUserByEmail(String email);
-    List<User> getUsersByNameAndEmail(String name, String email);
+
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    List<User> getUsersByName(@Param("name") String name);
+
+    @Query("SELECT u FROM User u WHERE u.email LIKE %:email%")
+    List<User> getUsersByEmail(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name% AND u.email LIKE %:email%")
+    List<User> getUsersByNameAndEmail(@Param("name") String name, @Param("email") String email);
 }
